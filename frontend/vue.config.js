8376c370041772719e7c27a1a51218259815b68e
Shikasembei <banbi.rice.cracker@gmail.com>
2021-06-18 23:25:52 +0900

    add/template

---
+++ frontend/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    pages: {
+      index: {
+        entry: "src/main.js",
+        title: "DRF Sample",
+      }
+    },
+    outputDir: "../static",
+    indexPath: "../templates/index.html",
+    publicPath: process.env.NODE_ENV === "production"
+      ? "/static/"
+      : "/"
+  }
+  
